c072810f06c05e54da945aedd65437e24b411d17
Nobodyworld <127373451+Nobodyworld@users.noreply.github.com>
2024-06-23 13:28:16 -0500

    Reset migrations and pycache

--- staticfiles/js/common/animations.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const animatedElements = document.querySelectorAll('.animated');
-
-    animatedElements.forEach((element) => {
-        element.classList.add('animate__animated');
-    });
-});
--- staticfiles/js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var preloader = document.getElementById('preloader');
-    if (preloader) {
-        preloader.style.display = 'none';
-    }
-});
